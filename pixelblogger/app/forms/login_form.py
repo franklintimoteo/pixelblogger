21b46f6a043a5078b101a041920d309e11c2bfc7
franklintimoteo <franklintimoteo@live.com>
2021-07-29 19:53:23 +0000

    Fix redundant form

--- pixelblogger/app/forms/login_form.py
+++ pixelblogger/app/forms/login_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import Email
-
+from wtforms import PasswordField, StringField, SubmitField, BooleanField
+from wtforms.validators import Email, DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[Email()])
-    password = PasswordField("Password")
-    btnSubmit = SubmitField("Login")
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(1, 64)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log in')
